ae8fe903057aa92c3f5f21c3acebbc1415fde935
Bai Li <bai@cs.toronto.edu>
2018-03-08 13:58:26 -0500

    Add ending marker to wordvec sequence, stack another linear layer

--- deeplearn/deep_main.py
+++ deeplearn/deep_main.py
@@ -6,6 +6,7 @@ import pandas as pd
 from torch.autograd import Variable
 from collections import OrderedDict
 import spacy
+import statistics
 import pdb
 
 
@@ -13,13 +14,15 @@ import pdb
 HIDDEN_SIZE = 1000
 
 # Size of word embeddings
-WORDVEC_SIZE = 300
+WORDVEC_SIZE = 300 + 1
+END_MARKER = np.zeros(WORDVEC_SIZE)
+END_MARKER[-1] = 1
 
 # Number of label classes
 NUM_LABELS = 6
 
 BATCH_SIZE = 500
-NUM_EPOCHS = 3
+NUM_EPOCHS = 4
 
 CATEGORIES = 'toxic severe_toxic obscene threat insult identity_hate'.split()
 
@@ -31,14 +34,21 @@ class DeepClassifier(nn.Module):
   def __init__(self):
     super(DeepClassifier, self).__init__()
 
-    self.nlp = spacy.load('en_core_web_md')
+    self.nlp = spacy.load('en_core_web_md', disable = ['ner', 'parser', 'tagger'])
     
     self.lstm = nn.LSTM(
       input_size = WORDVEC_SIZE,
       hidden_size = HIDDEN_SIZE,
       batch_first = True,
     )
-    self.multi_classifier = nn.Linear(HIDDEN_SIZE, NUM_LABELS)
+
+    # Put two layers before classification
+    self.multi_classifier = nn.Sequential(
+      nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE),
+      nn.Dropout(p = 0.5),
+      nn.ReLU(),
+      nn.Linear(HIDDEN_SIZE, NUM_LABELS),
+    )
 
 
   def forward(self, text):
@@ -47,7 +57,11 @@ class DeepClassifier(nn.Module):
     wordvec_sequence = []
     tokens = self.nlp(text)
     for tok in tokens:
-      wordvec_sequence.append(tok.vector)
+      v = tok.vector
+      v = np.append(v, [0])
+      wordvec_sequence.append(v)
+    wordvec_sequence.append(END_MARKER)
+
     wordvec_sequence = np.vstack(wordvec_sequence)
     wordvec_sequence = Variable(torch.Tensor(wordvec_sequence)).cuda()
     wordvec_sequence = wordvec_sequence.unsqueeze(0)
@@ -70,6 +84,7 @@ def do_train():
   train_data = pd.read_csv('../train.csv')
 
   model = DeepClassifier().cuda()
+  model.train()
   optimizer = optim.Adam(model.parameters(), lr = 0.0001)
   loss_fn = nn.MSELoss()
   torch.save(model.state_dict(), 'model.t7')
@@ -90,17 +105,42 @@ def do_train():
       if ix > 0 and ix % BATCH_SIZE == 0:
         batch_loss.backward()
         optimizer.step()
-        print(ix, float(batch_loss))
+        print('Epoch %d, iter %d, loss %0.9f' % (epoch, ix, float(batch_loss)))
         batch_loss = 0
 
     torch.save(model.state_dict(), 'model_epoch_%d.t7' % epoch)
+
+
+def do_train_validation():
+  """Compute training loss for model"""
+  train_data = pd.read_csv('../train.csv')
+
+  model = DeepClassifier().cuda()
+  model.eval()
+  model.load_state_dict(torch.load('model_lstm_lb_9433.t7'))
+  loss_fn = nn.MSELoss()
+
+  losses = []
+  for ix, row in train_data.iterrows():
+    if ix % 100 == 1:
+      # Feel free to stop this whenever
+      print(ix, statistics.mean(losses))
+
+    text = row['comment_text']
+    truth = Variable(torch.Tensor(row[2:8])).cuda()
+
+    out = model(text)
+    loss = float(loss_fn(out, truth))
+    losses.append(loss)
+
 
 
 def do_test():
   test_data = pd.read_csv('../test.csv')
 
   model = DeepClassifier().cuda()
-  model.load_state_dict(torch.load('model_epoch_2.t7'))
+  model.eval()
+  model.load_state_dict(torch.load('model_epoch_3.t7'))
 
   out = []
   for ix, row in test_data.iterrows():
@@ -119,5 +159,6 @@ def do_test():
 
 
 
-#do_train()
-do_test()
+do_train()
+#do_train_validation()
+#do_test()
